0676ccdf21f237b54031a241d7eea183dd766117
nutcas3 <mauricee423@gmail.com>
2024-07-08 23:20:44 +0300

    used deps

--- kafka/src/main.rs
+++ kafka/src/main.rs
@@ -1,4 +1,9 @@
-use tokio::io::{AsyncWriteExt}
+use rdkafka::{ClientConfig, Message};
+use rdkafka::consumer::{Consumer, StreamConsumer};
+use rdkafka::producer::{FutureProducer, FutureRecord};
+use rdkafka::util::Timeout;
+use tokio::io::{AsyncBufReadExt,AsyncWriteExt,BufReader}
+
 
 #[tokio::main]
 async fn main() {
