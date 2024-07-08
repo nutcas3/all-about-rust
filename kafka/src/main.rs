89a1b357bb951e4a1e28d419c137da535ab35823
nutcas3 <mauricee423@gmail.com>
2024-07-08 23:22:15 +0300

    added uuid

--- kafka/src/main.rs
+++ kafka/src/main.rs
@@ -1,9 +1,10 @@
+use std::env::args;
 use rdkafka::{ClientConfig, Message};
 use rdkafka::consumer::{Consumer, StreamConsumer};
 use rdkafka::producer::{FutureProducer, FutureRecord};
 use rdkafka::util::Timeout;
 use tokio::io::{AsyncBufReadExt,AsyncWriteExt,BufReader}
-
+use uuid::{Uuid};
 
 #[tokio::main]
 async fn main() {
