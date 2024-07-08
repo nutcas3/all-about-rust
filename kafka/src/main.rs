96e2c96d6a0b41b5e6b4705140f2da5c68b37772
nutcas3 <mauricee423@gmail.com>
2024-07-08 10:26:39 +0300

    added cargo lock to gitignore

--- kafka/src/main.rs
+++ kafka/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{AsyncWriteExt}
+
+#[tokio::main]
+async fn main() {
+    let mut stdout = tokio::io::stdout();
+
+    stdout.write(b"Welcome to Kafka!\n").await.unwrap();
+
 }
